fc99c5082e2d1705ba68c1fc056432e3690dc837
Justin Lam <jlam123321@gmail.com>
2025-06-22 21:51:05 -0700

    added base trigger
    still need to implement

---
+++ Scripts/NPCAi/NPCAiBaseTrigger.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+/*
+ * Author: [Lam, Justin]
+ * Last Updated: [06/22/2025]
+ * [triggers the ai to switch state for the state machine]
+ */
+
+public partial class NPCAiBaseTrigger : Node
+{
+}
