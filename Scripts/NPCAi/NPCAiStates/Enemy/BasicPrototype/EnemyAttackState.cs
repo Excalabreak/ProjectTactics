3efe0867d0dbcc0922531a5776e0dffad2ec4eb6
Justin Lam <jlam123321@gmail.com>
2025-07-01 12:10:20 -0700

    enemy units know which tile to move to

--- Scripts/NPCAi/NPCAiStates/Enemy/BasicPrototype/EnemyAttackState.cs
+++ Scripts/NPCAi/NPCAiStates/Enemy/BasicPrototype/EnemyAttackState.cs
@@ -1,10 +1,11 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 /*
  * Author: [Lam, Justin]
- * Last Updated: [06/27/2025]
+ * Last Updated: [07/01/2025]
  * [attack state for enemy ai
  * NOTE: very basic for now]
  */
@@ -17,6 +18,43 @@ public partial class EnemyAttackState : NPCAiState
     /// </summary>
     public override void TurnLogic()
     {
-        GD.Print(stateMachine.gameBoard.ClosestUnitPosition(UnitGroupEnum.PLAYER, stateMachine.unit.cell));
+        Vector2 target = stateMachine.gameBoard.ClosestUnitPosition(UnitGroupEnum.PLAYER, stateMachine.unit.cell);
+
+        List<Vector2> path = new List<Vector2>();
+        path.AddRange(stateMachine.gameBoard.DijkstraPathFinding(stateMachine.unit.cell, target));
+        path.RemoveAt(0);
+        path.RemoveAt(path.Count - 1);
+
+        if (path.Count > 0)
+        {
+            MoveLogic(path.ToArray(), stateMachine.unit.unitStats.currentMove);
+        }
+
+    }
+
+    private void MoveLogic(Vector2[] path, float moveLimit)
+    {
+        float currentMoveCost = 0;
+        int validIndex = -1;
+
+        for (int i = 0; i < path.Length; i++)
+        {
+            if (stateMachine.gameBoard.IsOccupied(path[i]))
+            {
+                break;
+            }
+            currentMoveCost += stateMachine.gameBoard.map.GetTileMoveCost(path[i]);
+            if (currentMoveCost > moveLimit)
+            {
+                break;
+            }
+
+            validIndex = i;
+        }
+
+        if (validIndex != -1)
+        {
+            GD.Print("Enemy moves to: " + path[validIndex]);
+        }
     }
 }
