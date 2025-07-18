6dd0bfe0fee04f77eb070a4a100927d9b75603e7
Justin Lam <jlam123321@gmail.com>
2025-07-18 13:01:17 -0700

    start menu for prototype

---
+++ Scripts/Playtest/PrototypeStartMenu.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+/*
+ * Author: [Lam, Justin]
+ * Last Updated: [07/18/2025]
+ * [start menu for playtest]
+ */
+
+public partial class PrototypeStartMenu : CanvasLayer
+{
+    [Export] private GridCursor _gridCursor;
+    [Export] private Button _cursorDefaultStartButton;
+
+    /// <summary>
+    /// sets up needed components
+    /// </summary>
+    public override void _Ready()
+    {
+        _cursorDefaultStartButton.GrabFocus();
+
+        //disables cursor
+        _gridCursor.Hide();
+        _gridCursor.ProcessMode = ProcessModeEnum.Disabled;
+    }
+
+    /// <summary>
+    /// hides the menu by queue free
+    /// </summary>
+    public void HideMenu()
+    {
+        _gridCursor.ProcessMode = ProcessModeEnum.Inherit;
+        _gridCursor.ResetCursor();
+        _gridCursor.Show();
+        QueueFree();
+    }
+}
