6e111364bc8e3989ee10ea57ae65506a45a45033
Justin Lam <jlam123321@gmail.com>
2025-08-15 13:04:38 -0700

    created resource for weapon
    can't do database if weapons have special effects

---
+++ Scripts/Items/Weapons/WeaponResource.cs
@@ -0,0 +1,35 @@
+using Godot;
+using Godot.Collections;
+using System;
+
+/*
+ * Author: [Lam, Justin]
+ * Last Updated: [08/15/2025]
+ * [resource for weapon]
+ */
+
+[GlobalClass]
+public partial class WeaponResource : Resource
+{
+    [Export] private string _weaponName;
+    [Export] private string _description;
+
+    //make enum for damage type
+    [Export] private Dictionary<string, int> _damage;
+
+    [Export] private int _minRange;
+    [Export] private int _maxRange;
+
+    [Export] private int _handling;
+    [Export] private int _critChance;
+
+    [Export] private int _weight;
+
+    [Export] private int _size;
+
+    //for special menu action
+    //[Export] private SpecialWeaponAction _specialAction
+
+    //for passive buffs
+    //[Export] private ItemPassiveBuff _passiveBuff
+}
