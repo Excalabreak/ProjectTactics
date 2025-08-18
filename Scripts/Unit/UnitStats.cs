a27e9a39e44a5bcdea62b6c5da30330ef757d8e9
Justin Lam <jlam123321@gmail.com>
2025-08-18 13:04:40 -0700

    stats now have combat stats

--- Scripts/Unit/UnitStats.cs
+++ Scripts/Unit/UnitStats.cs
@@ -12,6 +12,7 @@ public partial class UnitStats : Node
 {
     [Export] private Unit _unit;
     [Export] private BaseStats _baseStats;
+    [Export] private UnitInventory _unitInventory;
     //temp, make pretty later
 
     [Export] private Label _healthLable;
@@ -66,6 +67,79 @@ public partial class UnitStats : Node
         output += _baseStats.GetBaseStat(stat);
         return output;
     }
+
+    /// <summary>
+    /// the attack of the unit 
+    /// (aka damage if unit has no buffs and opponent has no def/res)
+    /// </summary>
+    public int attack
+    {
+        get
+        {
+            if (_unitInventory.equiptWeapon.IsPhysical())
+            {
+                return _baseStats.GetBaseStat(UnitStatEnum.STRENGTH) + _unitInventory.equiptWeapon.damage;
+            }
+            return _baseStats.GetBaseStat(UnitStatEnum.MAGIC) + _unitInventory.equiptWeapon.damage;
+        }
+    }
+
+    /// <summary>
+    /// base hit rate of hitting unit
+    /// </summary>
+    public int hitRate
+    {
+        get
+        {
+            return _unitInventory.equiptWeapon.handling + (_baseStats.GetBaseStat(UnitStatEnum.STRENGTH) * 3 / 2);
+        }
+    }
+
+    /// <summary>
+    /// base hit rate of hitting unit
+    /// </summary>
+    public int critRate
+    {
+        get
+        {
+            return _unitInventory.equiptWeapon.handling + (_baseStats.GetBaseStat(UnitStatEnum.DEXTERITY) / 2);
+        }
+    }
+
+    /// <summary>
+    /// how much to take off of hit rate
+    /// </summary>
+    public int avoid
+    {
+        get
+        {
+            return _baseStats.GetBaseStat(UnitStatEnum.STRENGTH) * 3 / 2;
+        }
+    }
+
+    /// <summary>
+    /// how much physical damage gets removed
+    /// </summary>
+    public int protection
+    {
+        get
+        {
+            return _baseStats.GetBaseStat(UnitStatEnum.DEFENSE);
+        }
+    }
+
+    /// <summary>
+    /// how much magical damage gets removed
+    /// </summary>
+    public int resilience
+    {
+        get
+        {
+            return _baseStats.GetBaseStat(UnitStatEnum.RESISTANCE);
+        }
+    }
+
+    //simple getters
 
     public int maxHP
     {
