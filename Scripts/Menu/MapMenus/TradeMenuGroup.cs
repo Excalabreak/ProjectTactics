1d859479cb5235daa3ea04ab280cc8fadaa5b093
Justin Lam <jlam123321@gmail.com>
2025-08-27 16:15:44 -0700

    player can now select items in trade menu

--- Scripts/Menu/MapMenus/TradeMenuGroup.cs
+++ Scripts/Menu/MapMenus/TradeMenuGroup.cs
@@ -14,6 +14,8 @@ public partial class TradeMenuGroup : Node
     [Export] private Button _equippedWeaponButton;
     [Export] private Button[] _itemSlotButtons;
 
+    private Button _currentSelectedButton;
+
     public void SetUpGroup(Unit unit)
     {
         UnitInventory inventory = unit.unitInventory;
@@ -34,8 +36,28 @@ public partial class TradeMenuGroup : Node
         }
     }
 
+    /// <summary>
+    /// toggles which item is selected
+    /// </summary>
+    /// <param name="toggle">what state is the button toggled to</param>
+    /// <param name="button">node path to button</param>
     private void ToggleItem(bool toggle, NodePath button)
     {
+        if (!toggle)
+        {
+            _currentSelectedButton = null;
+            return;
+        }
 
+        if (_currentSelectedButton != null)
+        {
+            _currentSelectedButton.ButtonPressed = false;
+        }
+
+        GD.Print(GetNode(button).Name);
+
+        Button toggledButton = GetNode(button) as Button;
+        _currentSelectedButton = toggledButton;
+        //check to trade item
     }
 }
