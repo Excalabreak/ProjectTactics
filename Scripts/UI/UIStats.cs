c275f57b9b9e4b00da10e23f2211d0c04a0e0d43
Justin Lam <jlam123321@gmail.com>
2025-09-10 12:47:13 -0700

    now shows all stats

--- Scripts/UI/UIStats.cs
+++ Scripts/UI/UIStats.cs
@@ -9,11 +9,7 @@ using System;
 
 public partial class UIStats : Control
 {
-    [Export] private Label _HPLable;
-    [Export] private Label _MOVLable;
-    [Export] private Label _ATKLable;
-    [Export] private Label _DEFLable;
-    [Export] private Label _RNGLable;
+    [Export] private Label _statsLable;
 
     /// <summary>
     /// displays the stats of a unit
@@ -21,19 +17,29 @@ public partial class UIStats : Control
     /// <param name="unit"></param>
     public void ShowUnitStats(Unit unit)
     {
-        _HPLable.Text = "HP: " + unit.unitStats.currentHP + "/" + unit.unitStats.maxHP;
-        _MOVLable.Text = "MOV: " + unit.unitActionEconomy.currentMove + "/" + unit.unitActionEconomy.maxMove;
-        _ATKLable.Text = "ATK: " + unit.unitStats.GetStat(UnitStatEnum.STRENGTH);
-        _DEFLable.Text = "DEF: " + unit.unitStats.GetStat(UnitStatEnum.DEFENSE);
+        string statsText = statsText = "HP: " + unit.unitStats.currentHP + "/" + unit.unitStats.maxHP;
+
+        statsText += "\nSTR: " + unit.unitStats.GetStat(UnitStatEnum.STRENGTH);
+        statsText += "\nMAG: " + unit.unitStats.GetStat(UnitStatEnum.MAGIC);
+        statsText += "\nDEX: " + unit.unitStats.GetStat(UnitStatEnum.DEXTERITY);
+        statsText += "\nSPD: " + unit.unitStats.GetStat(UnitStatEnum.SPEED);
+        statsText += "\nDEF: " + unit.unitStats.GetStat(UnitStatEnum.DEFENSE);
+        statsText += "\nRES: " + unit.unitStats.GetStat(UnitStatEnum.RESISTANCE);
+
+        statsText += "\n\nMOV: " + unit.unitActionEconomy.currentMove + "/" + unit.unitActionEconomy.maxMove;
 
         if (unit.unitInventory.equiptWeapon.minRange == unit.unitInventory.equiptWeapon.maxRange)
         {
-            _RNGLable.Text = "RANGE: " + unit.unitInventory.equiptWeapon.maxRange;
+            statsText += "\nRNG: " + unit.unitInventory.equiptWeapon.maxRange;
         }
         else
         {
-            _RNGLable.Text = "RANGE: " + unit.unitInventory.equiptWeapon.minRange + " - " + unit.unitInventory.equiptWeapon.maxRange;
+            statsText += "\nRNG: " + unit.unitInventory.equiptWeapon.minRange + " - " + unit.unitInventory.equiptWeapon.maxRange;
         }
+
+        statsText += "\nVIS: " + unit.unitStats.GetStat(UnitStatEnum.VISION);
+
+        _statsLable.Text = statsText;
 
         this.Visible = true;
     }
